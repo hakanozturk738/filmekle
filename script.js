2400d23feee3ad107bfff8bb6c4cb24b75d3757d
hakanozturk738 <finansalkasa@gmail.com>
2025-11-12 12:03:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,12 +9,10 @@ function validateForm() {
 
     // Doğru kullanıcı adı ve şifreyi kontrol et
     if (username === validUsername && password === validPassword) {
-        // Giriş başarılıysa formu gizle ve AppSheet iframe'ini göster
+        // Giriş başarılıysa formu gizle ve AppSheet yeni bir pencerede aç
         document.getElementById('form-container').style.display = "none";
-        window.open("https://www.appsheet.com/start/4ae2c219-2ac4-461f-908d-9f29b3f3092e", "_blank", "width=100%,height=100%");
+        window.open("https://www.appsheet.com/start/4ae2c219-2ac4-461f-908d-9f29b3f3092e", "_blank", "width=100%,height=100%");  // AppSheet'in URL'si
         return false; // Formun sayfayı yeniden yüklemesini engelle
     } else {
         alert("Kullanıcı adı veya şifre yanlış.");
-        return false; // Formun gönderilmesini engelle
-    }
-}
+        return false; // Formun gönderil
